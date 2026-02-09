2a144cbaa59416ac4873ff31c96acb7defb2efca
rjvanaken <rjvanaken@gmail.com>
2026-02-09 15:28:46 -0500

    shifting tributes back at starting position and display logic

--- Game.py
+++ Game.py
@@ -17,60 +17,83 @@ class Game:
         id = 0
         
         # top row - 6 tributes (skip corners at -4 and +3)
-        row = center_row - 3
+        row = center_row - 4
         for col in range(center_col - 2, center_col + 4):
             tribute = Tribute(id, (row, col))
             self.tributes.append(tribute)
             id += 1
         
         # right column - 6 tributes (skip corners at -4 and +3)
-        col = center_col + 4
+        col = center_col + 5
         for row in range(center_row - 2, center_row + 4):
             tribute = Tribute(id, (row, col))
             self.tributes.append(tribute)
             id += 1
         
         # bottom row - 6 tributes (skip corners at +3 and -4)
-        row = center_row + 4
+        row = center_row + 5
         for col in range(center_col + 3, center_col - 3, -1):
             tribute = Tribute(id, (row, col))
             self.tributes.append(tribute)
             id += 1
         
         # left column - 6 tributes (skip corners at +3 and -4)
-        col = center_col - 3
+        col = center_col - 4
         for row in range(center_row + 3, center_row - 3, -1):
             tribute = Tribute(id, (row, col))
             self.tributes.append(tribute)
             id += 1
     
 
-    # TODO do json import and then simplify display function
     def displayGrid(self):
         for i in range(self.arena.size):
             for j in range(self.arena.size):
-                # Check resources
-                resource_found = False
-                for resource in self.arena.resources:
-                    if resource.pos == (i, j):
-                        print(resource.type.value, end=' ')
-                        resource_found = True
-                        break
-                if resource_found:
-                    continue
-                # Check tributes
-                tribute_found = False
-                for tribute in self.tributes:
-                    if tribute.pos == (i, j):
-                        print(tribute.letter, end=' ')
-                        tribute_found = True
-                        break
-                if tribute_found:
-                    continue
-                    
-                # Nothing found
-                print('.', end=' ')
+                cell_value = self.arena.arena_grid[i][j]
+                
+                if cell_value == 0:
+                    print('.', end=' ')
+                else:
+                    print(cell_value, end=' ')
             print()  # New line after each row
+
+
+
+
+    # # TODO do json import and then simplify display function
+    # def displayGrid(self):
+    #     for i in range(self.arena.size):
+    #         for j in range(self.arena.size):
+    #             # Check resources
+    #             resource_found = False
+    #             for resource in self.arena.resources:
+    #                 if resource.pos == (i, j):
+    #                     print(resource.type.value, end=' ')
+    #                     resource_found = True
+    #                     break
+    #             if resource_found:
+    #                 continue
+    #             # Check obstacles
+    #             obstacle_found = False
+    #             for pos in self.arena.obstacles:
+    #                 if pos == (i, j):
+    #                     print('8', end=' ')
+    #                     obstacle_found = True
+    #                     break
+    #             if obstacle_found:
+    #                 continue
+    #             # Check tributes
+    #             tribute_found = False
+    #             for tribute in self.tributes:
+    #                 if tribute.pos == (i, j):
+    #                     print(tribute.letter, end=' ')
+    #                     tribute_found = True
+    #                     break
+    #             if tribute_found:
+    #                 continue
+                    
+    #             # Nothing found
+    #             print('.', end=' ')
+    #         print()  # New line after each row
 
 
         
