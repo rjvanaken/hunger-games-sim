06d165788ba6a4c2c18d5cf165174abe28f18e34
rjvanaken <rjvanaken@gmail.com>
2026-03-24 11:20:49 -0400

    gamemaker

---
+++ Gamemaker.py
@@ -0,0 +1,24 @@
+class Gamemaker:
+
+    def __init__(self, arena):
+        self.arena = arena
+
+
+    def shrinkArena():
+        pass
+
+        # find the outer most tribute and block everything from the 
+        # corner to that column on both sides, excluding the bottom center and cornucopia.
+
+
+    def activateMutt(mutt):
+        mutt.isDormant = False
+
+    def deployBomb(pos):
+        # note areas around it lose x health points
+        # note areas further around (maybe in the outer circle) lose fewer health points
+        # note areas around the other ring lose very smallish points
+        # everyone else safe
+        pass
+
+    
