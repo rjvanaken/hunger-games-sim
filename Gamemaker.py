db9ea8cb67d3f3dc056d9b10c90e6052fe85af64
rjvanaken <rjvanaken@gmail.com>
2026-03-24 14:18:19 -0400

    gamemaker influence functions

--- Gamemaker.py
+++ Gamemaker.py
@@ -1,24 +1,52 @@
+from config import BOMB_DAMAGE_FAR, BOMB_DAMAGE_NEAR
+
 class Gamemaker:
 
     def __init__(self, arena):
         self.arena = arena
 
 
-    def shrinkArena():
-        pass
+    def shrinkArena(self):
+        min_row = self.getShrinkRow()
+        min_col, max_col = self.getShrinkColumns()
 
-        # find the outer most tribute and block everything from the 
-        # corner to that column on both sides, excluding the bottom center and cornucopia.
-
+        for i in range(self.size):
+            for j in range(self.size):
+                if i < min_row:
+                    self.arena.arena_grid[i][j] = 8
+                if j < min_col or j > max_col:
+                    self.arena.arena_grid[i][j] = 8
+                
 
     def activateMutt(mutt):
         mutt.isDormant = False
 
-    def deployBomb(pos):
-        # note areas around it lose x health points
-        # note areas further around (maybe in the outer circle) lose fewer health points
-        # note areas around the other ring lose very smallish points
-        # everyone else safe
-        pass
+
+    def deployBomb(self, pos):
+        row, col = pos
+        for tribute in self.arena.tributes:
+            dr = abs(tribute.pos[0] - row)
+            dc = abs(tribute.pos[1] - col)
+            distance = max(dr, dc) 
+            
+            if distance == 0:
+                tribute.health = 0 
+            elif distance == 1:
+                tribute.health -= BOMB_DAMAGE_NEAR
+            elif distance == 2:
+                tribute.health -= BOMB_DAMAGE_FAR
+    
+
+
+
+    def getShrinkColumns(self):
+        min = min(tribute.pos[1] for tribute in self.arena.tributes)
+        max = max(tribute.pos[1] for tribute in self.arena.tributes)
+
+        return min, max
+             
+
+    def getShrinkRow(self):
+        return min(tribute.pos[0] for tribute in self.arena.tributes)
 
     
